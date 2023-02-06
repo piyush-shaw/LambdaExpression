57a1059b6d1332f9906c5df63b25887faab9e86e
piyush-shaw <piyushshaw0000@gmail.com>
2023-02-06 16:49:37 +0530

    [Piyush] added lambda expression class demo program

---
+++ LambdaExpressionDemo/LambdaExpressionDemo/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LambdaExpressionDemo
+{
+	public class Person
+	{
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+
+
+        public Person(string ssn, string name, string addr, int age)
+        {
+            SSN = ssn;
+            Name = name;
+            Address = addr;
+            Age = age;
+        }
+
+    }
+}
+
---
+++ LambdaExpressionDemo/LambdaExpressionDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using LambdaExpressionDemo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        List<Person> listPersonInCity = new List<Person>();
+        LambdaDemo.AddRecords(listPersonInCity);
+        LambdaDemo.Retrieving_TopTwoRecord_ForAgels_LessThanSixty(listPersonInCity);
+        LambdaDemo.CheckingForTeenagerPerson(listPersonInCity);
+        LambdaDemo.AllPersonsAverageAge(listPersonInCity);
+        LambdaDemo.CheckNameExistOrNot(listPersonInCity);
+        Console.ReadKey();
+    }
+}
